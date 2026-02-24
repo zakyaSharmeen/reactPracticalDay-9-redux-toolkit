354c9479bac9e89890a3108da3da2f76f1a0a971
zakyaSharmeen <zakyasharmeen@gmail.com>
2026-02-24 20:51:23 +0530

    added

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,8 +4,8 @@ import { MdFastfood } from "react-icons/md";
 function Nav() {
   return (
     <>
-      <div className="w-full h-25">
-        <div>
+      <div className="w-full h-[100px">
+        <div className="w-[60px] h-[60px] bg-white flex justify-center items-center border-4">
           <MdFastfood />
         </div>
       </div>
